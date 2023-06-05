ffb05eb1d26d83400fe105ae7efbce4fe1b7ffb8
iworld1991 <twang80@jhu.edu>
2023-06-05 14:21:46 -0400

    new IRFs in belief responses

--- model/BeliefDynamics-IRF.py
+++ model/BeliefDynamics-IRF.py
@@ -20,26 +20,35 @@
 # - This notebook explores various more generic implications of the social-network-based learning in _Learning from Friends in a Pandemic_ by Christos Makridis and Tao Wang. These results are not included in the paper.
 
 import numpy as np
-import pandas as pd
-from interpolation import interp, mlinterp
-from scipy import interpolate
-from numba import njit, float64
-from numba.experimental import jitclass
 import matplotlib as mp
 import matplotlib.pyplot as plt
-# %matplotlib inline
+# #%matplotlib inline
 
 
-# + {"code_folding": []}
+
+# + {"code_folding": [0]}
 ## figures configurations
+plt.style.use('seaborn')
+plt.rcParams["font.family"] = "Times New Roman" #'serif'
+plt.rcParams['font.serif'] = 'Ubuntu'
+plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+plt.rcParams['axes.labelweight'] = 'bold'
 
-mp.rc('xtick', labelsize=14) 
-mp.rc('ytick', labelsize=14) 
-
-
-fontsize = 14
-legendsize = 12
-linewith= 3.0
+## Set the 
+plt.rc('font', size=25)
+# Set the axes title font size
+plt.rc('axes', titlesize=20)
+# Set the axes labels font size
+plt.rc('axes', labelsize=20)
+# Set the font size for x tick labels
+plt.rc('xtick', labelsize=20)
+# Set the font size for y tick labels
+plt.rc('ytick', labelsize=20)
+# Set the legend font size
+plt.rc('legend', fontsize=20)
+# Set the font size of the figure title
+plt.rc('figure', titlesize=20)
+plt.rc('lines', linewidth=4)
 
 # + {"code_folding": []}
 ## load SCI weight matrix 
@@ -63,7 +72,7 @@ N16 = W16.shape[0]
 
 # + {"code_folding": [4, 29, 40]}
 ##############################################################
-## analytical functison of IRFs to an aggregate shock and idiosycratic shocks
+## analytical function of IRFs to an aggregate shock and idiosyncratic shocks
 #########################################################################
 
 def irf_ag(k,
@@ -106,7 +115,7 @@ def irf_id_c(k,
              λ, 
              W,  ## listening matrix 
              v,  ## steps of IRF
-             Z): ## new addition, a vector of 1/0s deciding wheather the shock hit there
+             Z): ## new addition, a vector of 1/0s deciding if the shock hit there
     
     N = len(W)
     H = np.ones(N)
@@ -128,72 +137,6 @@ def irf_id_c(k,
 
 # #### How does aggregate IRF depends on k and $\lambda$
 
-# + {"code_folding": [0]}
-## compute IRFs 
-
-"""
-grid_size = 20
-
-k_vals = np.linspace(0.001, 2, grid_size)
-λ_vals = np.linspace(0.001, 0.99, grid_size)
-
-## generate IRFs for different v
-vs = [0,1,2,3]
-
-IRF_ags_ls= []
-
-for v_id,v in enumerate(vs):
-    IRF_ags = np.empty((grid_size, grid_size))
-    for i, k in enumerate(k_vals):
-        for j, λ in enumerate(λ_vals):
-            #IRF_ags[i, j] = irf_ag(k,λ,W,v)
-            IRF_ags[i, j] = irf_ag_simple(k,
-                                          λ,
-                                          v)
-    IRF_ags_ls.append(IRF_ags)
-    
-"""
-
-# + {"code_folding": [0, 7]}
-#levels = np.array([0.0,0.2,0.4,0.6,0.8,1.0,1.5])
-
-"""
-## plot 
-fig, ax = plt.subplots(2,2,figsize=(13,10))
-ax = ax.ravel()
-
-for v_id,v in enumerate(vs):
-    this_IRF_ags = IRF_ags_ls[v_id]
-    cs1 = ax[v_id].contourf(k_vals, λ_vals, this_IRF_ags.T, alpha=0.2)
-    ctr1 = ax[v_id].contour(k_vals, λ_vals, this_IRF_ags.T)
-    plt.clabel(ctr1, inline=1, fontsize=13)
-    #plt.colorbar(cs1, ax=ax)
-    ax[v_id].set_title(r'IRF, v={}'.format(v))
-    ax[2].set_xlabel("$k$", fontsize=16)
-    ax[3].set_xlabel("$k$", fontsize=16)
-    ax[0].set_ylabel("$λ$", fontsize=16)
-    ax[2].set_ylabel("$λ$", fontsize=16)
-    
-    ax[v_id].ticklabel_format(useOffset=False)
-
-    ax[v_id].annotate('overreactive', xy=(1.8,0.1),
-                xytext=(1.0,0.1), va='center', multialignment='right',
-                arrowprops={'arrowstyle': '->', 'lw': 2, 'ec': 'r'})
-    
-    ax[v_id].annotate('rigid', xy=(0.1,0.1),
-                xytext=(0.4,0.1), va='center', multialignment='right',
-                arrowprops={'arrowstyle': '->', 'lw': 2, 'ec': 'r'})
-
-    ax[v_id].annotate('social', xy=(0.2,0.7),
-                xytext=(0.1,0.5), va='center', multialignment='right',
-                arrowprops={'arrowstyle': '->', 'lw': 2, 'ec': 'r'})
-    
-
-plt.savefig('../graph/model/IRF_ag_contourf.jpg')
-
-"""
-# -
-
 # #### The time path of the IRF
 
 k_fix = 0.33
@@ -201,7 +144,7 @@ k_vals = np.array([0.3,0.5,0.9])
 λ_vals = np.array([0.01,0.3,0.8,0.99])
 v_vals = np.arange(10)
 
-# + {"code_folding": [7]}
+# + {"code_folding": [2]}
 IRF_ag_path = np.empty((len(k_vals),len(λ_vals), len(v_vals)))
                        
 for j,k in enumerate(k_vals):
@@ -211,11 +154,11 @@ for j,k in enumerate(k_vals):
                                               λ,
                                               v)
 
-# + {"code_folding": [8]}
+# + {"code_folding": [6, 8]}
 lp_ls=['-','--','-.','v-']
 nb_plots = len(k_vals)
 
-fig,ax =plt.subplots(nb_plots,1,figsize=(7,10))
+fig,ax =plt.subplots(nb_plots,1,figsize=(8,12))
 ax = ax.ravel()
 
 for j,k in enumerate(k_vals):
@@ -253,7 +196,7 @@ def where_to_shock(W,
     return Z 
 
 
-# + {"code_folding": []}
+# + {"code_folding": [1, 3, 5]}
 ## generate Z vectors 
 Z_top = where_to_shock(W,
                       (0.0,0.33))
@@ -261,73 +204,6 @@ Z_mid = where_to_shock(W,
                       (0.33,0.66))
 Z_bot = where_to_shock(W,
                       (0.66,0.99))
-
-# + {"code_folding": []}
-"""
-## compute IRFs 
-
-grid_size = 20
-
-k_vals = np.linspace(0.001, 2, grid_size)
-λ_vals = np.linspace(0.001, 0.99, grid_size)
-
-
-## generate IRFs for different v
-vs = [0,1,2,3]
-IRF_ids_ls = []
-
-for v_id,v in enumerate(vs):
-    IRF_ids = np.empty((grid_size, grid_size))
-    for i, k in enumerate(k_vals):
-        for j, λ in enumerate(λ_vals):
-            IRF_ids[i, j] = irf_id_c(k=k,
-                                     λ=λ,
-                                     W=W,
-                                     v=v,
-                                    Z = Z_top)
-    IRF_ids_ls.append(IRF_ids)
-    
-"""
-
-# + {"code_folding": [7]}
-"""
-## plot 
-#levels = np.array([0.0,0.1,0.2,0.3,0.4,0.5])
-
-fig, ax = plt.subplots(2,2,figsize=(13,10))
-ax = ax.ravel()
-
-
-for v_id,v in enumerate(vs):
-    this_IRF_ids = IRF_ids_ls[v_id]
-    cs1 = ax[v_id].contourf(k_vals, λ_vals, this_IRF_ids.T, alpha=0.2)
-    ctr1 = ax[v_id].contour(k_vals, λ_vals, this_IRF_ids.T)
-    plt.clabel(ctr1, inline=2, fontsize=13)
-    #plt.colorbar(cs1, ax=ax)
-    ax[2].set_xlabel("$k$", fontsize=16)
-    ax[3].set_xlabel("$k$", fontsize=16)
-    ax[0].set_ylabel("$λ$", fontsize=16)
-    ax[2].set_ylabel("$λ$", fontsize=16)
-    ax[v_id].set_title(r'IRF, v={}'.format(v))
-
-    ax[v_id].ticklabel_format(useOffset=False)
-
-    ax[v_id].annotate('overreactive', xy=(1.8,0.1),
-                xytext=(1.0,0.1), va='center', multialignment='right',
-                arrowprops={'arrowstyle': '->', 'lw': 2, 'ec': 'r'})
-    
-    ax[v_id].annotate('rigid', xy=(0.1,0.1),
-                xytext=(0.4,0.1), va='center', multialignment='right',
-                arrowprops={'arrowstyle': '->', 'lw': 2, 'ec': 'r'})
-
-    ax[v_id].annotate('social', xy=(1.68,0.5),
-                xytext=(1.6,0.3), va='center', multialignment='right',
-                arrowprops={'arrowstyle': '->', 'lw': 2, 'ec': 'r'})
-
-plt.savefig('../graph/model/IRF_id_contourf.jpg')
-
-
-"""
 # -
 
 # #### Time path of IRF to local shocks to the top
@@ -355,7 +231,7 @@ nb_plots = len(k_vals)
 #b_lim =-0.1 ## y axis range lower lim
 #t_lim = 0.6  ## y axis range upper lim
 
-fig,ax =plt.subplots(nb_plots,1,figsize=(7,10))
+fig,ax =plt.subplots(nb_plots,1,figsize=(8,12))
 ax = ax.ravel()
 
 for j,k in enumerate(k_vals):
@@ -400,7 +276,7 @@ nb_plots = len(k_vals)
 #b_lim =-0.3 ## y axis range lower lim
 #t_lim = 0.6  ## y axis range upper lim
 
-fig,ax =plt.subplots(nb_plots,1,figsize=(7,10))
+fig,ax =plt.subplots(nb_plots,1,figsize=(8,12))
 ax = ax.ravel()
 
 for j,k in enumerate(k_vals):
@@ -424,7 +300,7 @@ plt.savefig('../graph/model/IRF_id_path_bot.jpg')
 autarky = np.identity(N)
 egal = np.ones((N,N))*1/N
 
-# + {"code_folding": [1]}
+# + {"code_folding": [0, 1, 16]}
 ## different networks 
 W_ls = [W,
         autarky,
@@ -441,20 +317,20 @@ k_vals = np.linspace(0.001, 2.00, grid_size)
 λ_vals = np.linspace(0.001, 0.99, grid_size)
 
 
-for s,weigt in enumerate(W_ls):
+for s,weight in enumerate(W_ls):
     IRF_ids = np.empty((grid_size, grid_size))
     for i, k in enumerate(k_vals):
         for j, λ in enumerate(λ_vals):
             IRF_ids[i, j] = irf_id_c(k=k,
                                      λ=λ,
-                                     W=weigt,
+                                     W=weight,
                                      v=1,
                                      Z = Z_top)
     # foreach weight matrix 
     IRF_ids_ls.append(IRF_ids)
 
 
-# + {"code_folding": []}
+# + {"code_folding": [0, 5]}
 ## plot 
 fig, ax = plt.subplots(1,3,figsize=(18,5))
 
@@ -520,7 +396,7 @@ nb_plots = len(k_vals)
 #b_lim =0.0 ## y axis range lower lim
 #t_lim = 0.6  ## y axis range upper lim
 
-fig,ax =plt.subplots(nb_plots,1,figsize=(7,10))
+fig,ax =plt.subplots(nb_plots,1,figsize=(8,12))
 ax = ax.ravel()
 
 for j,k in enumerate(k_vals):
@@ -537,3 +413,6 @@ for j,k in enumerate(k_vals):
         #ax[j].set_ylim(b_lim, t_lim)
 fig.tight_layout(pad=1.4)
 plt.savefig('../graph/model/IRF_id_path_top_2016.jpg')
+# -
+
+
